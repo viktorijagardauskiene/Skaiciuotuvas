8b1970ae872b79227c94b60c6750149e977bf5c4
viktorijagardauskiene <viktorija.gardauskiene@gmail.com>
2017-06-28 20:36:41 +0300

    sukurta forma su dviem kintamaisiais ir dropdown listu ir mygtuku

--- index.php
+++ index.php
@@ -4,6 +4,19 @@
 	<title>Skaiciuotuvas</title>
 </head>
 <body>
+	<form>
+		
+		<input type="text" name="argumentas1">
+		<select>
+			<option value="sum">Add</option>
+			<option value="sub">Subtract</option>
+			<option value="mul">Multiply</option>
+			<option value="divi">Divide</option>
+		</select>
+		<input type="text" name="argumentas2">
+		<input type="submit" value="Skaiciuok!">
+	</form>
+
 
 </body>
 </html>
